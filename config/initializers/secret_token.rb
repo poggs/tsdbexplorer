11913ccdfed94be071ba156b33fbb5dcc777cbc4
Peter Hicks <peter.hicks@poggs.co.uk>
2011-04-19 22:22:47 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsdbexplorer::Application.config.secret_token = '2bfc1cf71d6b00a2ffbf8f0effb92a33f0efa1c6fb70ea86dd3d007e21c697bf2b91c37098abe173188e1f168bef247a7e97c25196a1bc54151d38e6c07e817e'
