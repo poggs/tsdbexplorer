9749467bce6e29d60ad9d4c4f411f3e8a99f87cc
Peter Hicks <peter.hicks@poggs.co.uk>
2011-11-04 22:13:40 +0000

    Display a tick next to trains with real-time information available

--- app/models/basic_schedule.rb
+++ app/models/basic_schedule.rb
@@ -28,6 +28,10 @@ class BasicSchedule < ActiveRecord::Base
   scope :runs_on, lambda { |date| where('? BETWEEN runs_from AND runs_to', date).runs_on_wday(Date.parse(date).wday) }
   scope :all_schedules_by_uid, lambda { |train_uid| where(:train_uid => train_uid).order('stp_indicator DESC') }
 
+  def realtime_for(date)
+    DailySchedule.where(:train_uid => self.train_uid, :runs_on => date).first
+  end
+
   def origin
     self.locations.where(:location_type => 'LO').first
   end
