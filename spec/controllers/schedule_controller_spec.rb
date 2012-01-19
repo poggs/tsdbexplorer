752405c07224c52678bb4384df1d1e1a772b402e
Peter Hicks <peter.hicks@poggs.co.uk>
2012-01-19 16:47:50 +0000

    Fix the restriction spec by using a blatant hack

--- spec/controllers/schedule_controller_spec.rb
+++ spec/controllers/schedule_controller_spec.rb
@@ -241,11 +241,16 @@ describe ScheduleController do
   # Security
 
   it "should not allow a schedule included in the restricted category list to be looked up by schedule UID" do
-    $CONFIG['RESTRICTIONS']['category'] = ['ZZ']
+
+    # The default scope in BasicSchedule is set at startup via the config
+    # file, and isn't evaluated dynamically.  We're forced to change the
+    # default scope by hand.  A better way would be to reload the model
+    # after changing the $CONFIG['RESTRICTIONS'] variable
+
+    BasicSchedule.default_scopes = [{:conditions=>["category NOT IN (?)", ["ZZ"]]}]
     TSDBExplorer::CIF::process_cif_file('test/fixtures/cif/restricted_category.cif')
     get :schedule_by_uid_and_run_date, :uid => 'H10556', :year => '2011', :month => '12', :day => '11'
     response.code.should eql('404')
   end
-
 
 end
