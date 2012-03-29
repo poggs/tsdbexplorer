2ac313ef161942f086367cadf11574b887c2898a
Peter Hicks <peter.hicks@poggs.co.uk>
2012-03-29 12:06:30 +0100

    Add a session controller to manage session variables

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,68 @@
+#
+#  This file is part of TSDBExplorer.
+#
+#  TSDBExplorer is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  TSDBExplorer is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with TSDBExplorer.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  $Id$
+#
+
+require 'spec_helper'
+
+describe SessionController do
+
+  # Setting
+
+  it "should allow a specific variable to be set" do
+    get :set, :key => 'foo', :value => 'bar'
+    session['foo'].should eql('bar')
+  end
+
+  it "should allow a specific variable to be un-set" do
+    get :set, :key => 'foo', :value => 'bar'
+    session['foo'].should eql('bar')
+    get :clear, :key => 'foo'
+    session['foo'].should be_nil
+  end
+
+  it "should not raise an error if asked to un-set a variable that doesn't exist" do
+    get :clear, :key => 'foo'
+    response.should be_success
+    session['foo'].should be_nil
+  end
+
+
+  # Toggling
+
+  describe "Variable toggling" do
+
+    it "should toggle a nonexistent variable to true" do
+      get :toggle, :key => 'foo'
+      session['foo'].should be_true
+    end
+
+    it "should toggle a true value to false" do
+      session['foo'] = true
+      get :toggle, :key => 'foo'
+      session['foo'].should be_false
+    end
+
+    it "should toggle a false value to true" do
+      session['foo'] = false
+      get :toggle, :key => 'foo'
+      session['foo'].should be_true
+    end
+
+  end
+
+end
---
+++ spec/helpers/session_helper_spec.rb
@@ -0,0 +1,34 @@
+#
+#  This file is part of TSDBExplorer.
+#
+#  TSDBExplorer is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  TSDBExplorer is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+#  Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with TSDBExplorer.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  $Id$
+#
+
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SessionHelper. For example:
+#
+# describe SessionHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe SessionHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
