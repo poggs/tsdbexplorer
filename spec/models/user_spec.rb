914ff32ac7bb4a48389dcfa364301bd998249f9f
Peter Hicks <peter.hicks@poggs.co.uk>
2012-04-02 07:37:32 +0100

    Add specs for the User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,5 +20,32 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Validity
+
+  it "should not be valid when first created" do
+    u = User.new
+    u.should_not be_valid
+  end
+
+  it "should not be valid with only an email address" do
+    u = User.new(:email => 'nobody@example.com')
+    u.should_not be_valid
+  end
+
+  it "should not be valid with only a full name" do
+    u = User.new(:full_name => 'Full Name')
+    u.should_not be_valid
+  end
+
+  it "should not be valid with an email address, full name and a password of fewer than six characters" do
+    u = User.new(:email => 'nobody@example.com', :full_name => 'Full Name', :password => 'FOO')
+    u.should_not be_valid
+  end
+
+  it "should be valid with an email address, full name and a password of at least six characters" do
+    u = User.new(:email => 'nobody@example.com', :full_name => 'Full Name', :password => 'FOOBARBAZ')
+    u.should be_valid
+  end
+
 end
