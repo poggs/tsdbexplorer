6ba60a7eefda0c3379929886b33a4e1ad49a7a0b
Peter Hicks <peter.hicks@poggs.co.uk>
2012-06-18 10:28:51 +0100

    Remove Redis transaction as it's not supported

--- lib/tsdbexplorer/import.rb
+++ lib/tsdbexplorer/import.rb
@@ -27,63 +27,57 @@ module TSDBExplorer
 
       return Struct.new(:status, :message).new(:status => :error, :message => "Location data not found at #{filename}") unless File.readable?(filename)
 
-      # Wrap everything in a Redis transaction
+      old_tiplocs = $REDIS.keys("TIPLOC:*")
 
-      $REDIS.multi do
+      unless old_tiplocs.nil?
+        old_tiplocs.each do |t|
+          $REDIS.del(t)
+        end
+      end
 
-        old_tiplocs = $REDIS.keys("TIPLOC:*")
 
-        unless old_tiplocs.nil?
-          old_tiplocs.each do |t|
-            $REDIS.del(t)
-          end
+      old_name_to_tiploc = $REDIS.keys("NAME:TO-TIPLOC:*")
+
+      unless old_name_to_tiploc.nil?
+        old_name_to_tiploc.each do |tn|
+          $REDIS.del(tn)
+        end
+      end
+
+
+      old_stanox = $REDIS.keys("STANOX:*")
+
+      unless old_stanox.nil?
+        old_stanox.each do |s|
+          $REDIS.del(s)
+        end
+      end
+
+      location_records = Array.new
+
+      CSV.foreach(filename, { :col_sep => ";" }) do |location|
+
+        location_records << Point.new(:full_name => location[0], :short_name => location[1], :stanox => location[2], :stanme => location[3], :tiploc => location[5], :latitude => location[6], :longitude => location[7])
+
+        $REDIS.hset("TIPLOC:#{location[5]}", "full_name", location[0])
+        $REDIS.hset("TIPLOC:#{location[5]}", "short_name", location[0])
+        $REDIS.hset("TIPLOC:#{location[5]}", "stanme", location[3])
+
+        Text::Metaphone.metaphone(location[0]).split(' ').each do |m|
+          $REDIS.sadd("FUZZY-NAME:TO-TIPLOC:#{m}", location[5])
         end
 
+        $REDIS.set("NAME:TO-TIPLOC:#{TSDBExplorer::strip_and_upcase(location[0])}", location[5])
 
-        old_name_to_tiploc = $REDIS.keys("NAME:TO-TIPLOC:*")
-
-        unless old_name_to_tiploc.nil?
-          old_name_to_tiploc.each do |tn|
-            $REDIS.del(tn)
-          end
-        end
+        $REDIS.hset("STANOX:#{location[2]}", "tiploc", location[5])
+      end
 
 
-        old_stanox = $REDIS.keys("STANOX:*")
+      # Delete the on-disk records and import the new ones in a single transaction
 
-        unless old_stanox.nil?
-          old_stanox.each do |s|
-            $REDIS.del(s)
-          end
-        end
-
-        location_records = Array.new
-
-        CSV.foreach(filename, { :col_sep => ";" }) do |location|
-
-          location_records << Point.new(:full_name => location[0], :short_name => location[1], :stanox => location[2], :stanme => location[3], :tiploc => location[5], :latitude => location[6], :longitude => location[7])
-
-          $REDIS.hset("TIPLOC:#{location[5]}", "full_name", location[0])
-          $REDIS.hset("TIPLOC:#{location[5]}", "short_name", location[0])
-          $REDIS.hset("TIPLOC:#{location[5]}", "stanme", location[3])
-
-          Text::Metaphone.metaphone(location[0]).split(' ').each do |m|
-            $REDIS.sadd("FUZZY-NAME:TO-TIPLOC:#{m}", location[5])
-          end
-
-          $REDIS.set("NAME:TO-TIPLOC:#{TSDBExplorer::strip_and_upcase(location[0])}", location[5])
-
-          $REDIS.hset("STANOX:#{location[2]}", "tiploc", location[5])
-        end
-
-
-        # Delete the on-disk records and import the new ones in a single transaction
-
-        ActiveRecord::Base.transaction do
-          Point.delete_all
-          location_result = Point.import(location_records)
-        end
-
+      ActiveRecord::Base.transaction do
+        Point.delete_all
+        location_result = Point.import(location_records)
       end
 
       return Struct.new(:status, :message).new(:status => :ok, :message => "Import succesful")
@@ -97,46 +91,40 @@ module TSDBExplorer
 
       return Struct.new(:status, :message).new(:status => :error, :message => "CRS to TIPLOC data not found at #{filename}") unless File.readable?(filename)
 
-      # Wrap everything in a Redis transaction
+      old_crs_to_tiploc = $REDIS.keys("CRS:TO-TIPLOC:*")
 
-      $REDIS.multi do
-
-        old_crs_to_tiploc = $REDIS.keys("CRS:TO-TIPLOC:*")
-
-        unless old_crs_to_tiploc.nil?
-          old_crs_to_tiploc.each do |ct|
-            $REDIS.del(ct)
-          end
+      unless old_crs_to_tiploc.nil?
+        old_crs_to_tiploc.each do |ct|
+          $REDIS.del(ct)
         end
+      end
 
-        old_crs_to_name = $REDIS.keys("CRS:TO-NAME:*")
+      old_crs_to_name = $REDIS.keys("CRS:TO-NAME:*")
 
-        unless old_crs_to_name.nil?
-          old_crs_to_name.each do |cn|
-            $REDIS.del(cn)
-          end
+      unless old_crs_to_name.nil?
+        old_crs_to_name.each do |cn|
+          $REDIS.del(cn)
         end
+      end
 
-        old_name_to_crs = $REDIS.keys("NAME:TO-CRS:*")
+      old_name_to_crs = $REDIS.keys("NAME:TO-CRS:*")
 
-        unless old_name_to_crs.nil?
-          old_name_to_crs.each do |nc|
-            $REDIS.del(nc)
-          end
+      unless old_name_to_crs.nil?
+        old_name_to_crs.each do |nc|
+          $REDIS.del(nc)
         end
+      end
 
-        CSV.foreach(filename, { :col_sep => ";" }) do |crs_code|
-          crs_code[2].split(",").each do |tiploc|
-            $REDIS.sadd("CRS:TO-TIPLOC:#{crs_code[0]}", tiploc)
-            $REDIS.set("CRS:TO-NAME:#{crs_code[0]}", crs_code[1])
-            $REDIS.set("NAME:TO-CRS:#{TSDBExplorer::strip_and_upcase(crs_code[1])}", crs_code[0])
+      CSV.foreach(filename, { :col_sep => ";" }) do |crs_code|
+        crs_code[2].split(",").each do |tiploc|
+          $REDIS.sadd("CRS:TO-TIPLOC:#{crs_code[0]}", tiploc)
+          $REDIS.set("CRS:TO-NAME:#{crs_code[0]}", crs_code[1])
+          $REDIS.set("NAME:TO-CRS:#{TSDBExplorer::strip_and_upcase(crs_code[1])}", crs_code[0])
 #            $REDIS.set("FUZZY-NAME:TO-CRS:#{Text::Metaphone.metaphone(crs_code[1])}", crs_code[0])
 
-             Text::Metaphone.metaphone(crs_code[1]).split(' ').each do |m|
-               $REDIS.sadd("FUZZY-NAME:TO-CRS:#{m}", crs_code[0])
-             end
-
-          end
+           Text::Metaphone.metaphone(crs_code[1]).split(' ').each do |m|
+             $REDIS.sadd("FUZZY-NAME:TO-CRS:#{m}", crs_code[0])
+           end
 
         end
 
